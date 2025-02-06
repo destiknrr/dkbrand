4738dd25d410bebb9176d805667c7b13308672da
destiknrr <destinurdatillah@gmail.com>
2025-02-06 22:58:34 +0700

    Portofolio website

---
+++ script.js
@@ -0,0 +1,14 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+    }
+
+    function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+    }
+
+// Initialize AOS
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init();
+});
